39153293464d7f3aeba65e4215ea272d3a9d6cd3
MuskanD22 <52172087+MuskanD22@users.noreply.github.com>
2023-04-24 19:04:09 +0530

    Spark code with scala

--- test.scala
+++ test.scala
@@ -3,29 +3,6 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
 object test extends App {
-
-  /*val sparkConf=new SparkConf()
-  sparkConf.set("spark.app.name","Word Count")
-  sparkConf.set("spark.master","local[2]")
-
-  val spark=SparkSession.builder().config(sparkConf).getOrCreate()
-
-  val sc=spark.sparkContext;
-  //1. loading the data in memory in form of rdd
-  val loadDataRDD = sc.textFile("C:/Users/mohit/Desktop/BigDataCourse/week 10/6.datasets/samplefile.txt")
-  //res0: Array[String] = Array(spark is very intersting, spark is inmemoery compute engine, "", I hope you are learning well, I hope your learning experence with trendytech is great, "")
-
-  //split the sentense of word of array flat map basically take each line as input
-  val wordRdd=loadDataRDD.flatMap(x=> x.split(" ")) //array(array{hello, how, id},array--)
-
-  val assignNumberToWordRdd= wordRdd.map(x=> (x,1))  //(hello,1),() ----
-
-  val reduceToFindCountRdd= assignNumberToWordRdd.reduceByKey((x,y)=> x+y) //In Spark, the reduceByKey function is a frequently used transformation operation that performs aggregation of data. It receives key-value pairs (K, V) as an input,
-                                                                          //aggregates the values based on the key and generates a dataset of (K, V) pairs as an output.
-
-  reduceToFindCountRdd.collect().foreach(println) //action
-
- */
   val sparkConf = new SparkConf()
   sparkConf.set("spark.app.name", "Booking ETL")
   sparkConf.set("spark.master", "local[2]")
@@ -37,7 +14,6 @@ object test extends App {
   // read the bookings data into a DataFrame
   val bookings = spark.read.format("csv")
     .option("header", "true")
-    .option("inferSchema", "true")
     .load("C:/Users/mohit/Desktop/HRS/Question2_Input.csv")
 
   // filter the bookings made by Tour Operators
