c4b3a885284c0eec2043117c9000837b174e16f9
MuskanD22 <52172087+MuskanD22@users.noreply.github.com>
2023-04-24 18:54:37 +0530

    Update booking.py

--- booking.py
+++ booking.py
@@ -10,23 +10,12 @@ bookings = spark.read.format("csv").option("header", "true").option("inferSchema
 # Filter the DataFrame to only include bookings where the Market Segment is designated as Tour Operators
 filtered_bookings = bookings.filter(col("market_segment") == "Tour Operators")
 
-#print(bookings.printSchema())
- #Create two new columns in the filtered DataFrame, arrival_date and departure_date
-#filtered_bookings = filtered_bookings.withColumn("arrival_date", concat_ws("-", col("arrival_date_year"), col("arrival_date_month"), col("arrival_date_day_of_month"))) \
- #                                     .withColumn("departure_date", date_add(col("arrival_date"), col("stays_in_weekend_nights") + col("stays_in_week_nights")))
-
 filtered_bookings = filtered_bookings.withColumn("arrival_date", concat_ws("-", col("arrival_date_year"), col("arrival_date_month"), col("arrival_date_day_of_month"))) \
                                      .withColumn("departure_date", expr("date_add(arrival_date, CAST(stays_in_weekend_nights AS INT) + CAST(stays_in_week_nights AS INT))"))
-
-                                                 #date_add(col("arrival_date"), (col("stays_in_weekend_nights") + col("stays_in_week_nights")).cast("int")))
 
 # Create a new column in the filtered DataFrame, with_family_breakfast
 filtered_bookings = filtered_bookings.withColumn("with_family_breakfast", when(col("children") + col("babies") > 0, "Yes").otherwise("No"))
 
 # Save the resulting DataFrame as a parquet file
-#filtered_bookings.write.csv("C:/Users/mohit/Desktop/HRS/output/filtered_bookings.csv")
-#filtered_bookings.to_parquet("C:/Users/mohit/Desktop/HRS/output/filtered_bookings.parquet")
-
-
 filtered_bookings.write.mode("overwrite").parquet("C:/Users/mohit/Desktop/HRS/output/filtered_bookings.parquet")
 spark.stop()
